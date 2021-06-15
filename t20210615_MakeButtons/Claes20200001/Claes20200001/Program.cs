695217872e9f9672d09245e195857bf7a2069549
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-06-15 12:47:08 +0900

    Voyager 1 is 22906830066.698 kilometers away from the Sun.

--- t20210615_MakeButtons/Claes20200001/Claes20200001/Program.cs
+++ t20210615_MakeButtons/Claes20200001/Claes20200001/Program.cs
@@ -48,7 +48,80 @@ namespace Charlotte
 
 		private void Main4()
 		{
-			// none
+			// -- choose one --
+
+			MakeButtons_20200001_DoremyRockman();
+			//MakeButtons_20200002_SSAGame();
+
+			// --
+		}
+
+		private void MakeButtons_20200001_DoremyRockman()
+		{
+			I4Color color = new I4Color(255, 64, 64, 255);
+
+			MakeButtons(2400, color, "ゲームスタート", 60);
+			MakeButtons(2400, color, "コンテニュー", 180);
+			MakeButtons(2400, color, "設定", 675);
+			MakeButtons(2400, color, "終了", 675);
+		}
+
+		private void MakeButtons_20200002_SSAGame()
+		{
+			I4Color color = new I4Color(233, 255, 33, 255);
+
+			MakeButtons(2400, color, "ゲームスタート", 60);
+			MakeButtons(2400, color, "コンテニュー", 180);
+			MakeButtons(2400, color, "設定", 675);
+			MakeButtons(2400, color, "終了", 675);
+		}
+
+		private void MakeButtons(int w, I4Color frameColor, string text, int text_x)
+		{
+			//int w = 2400;
+			int h = 480;
+			int frame = 60;
+
+			I4Color backColor = new I4Color(255, 255, 255, 0);
+			I4Color textColor = new I4Color(255, 255, 255, 255);
+
+			Canvas canvas = new Canvas(w, h);
+
+			canvas.Fill(backColor);
+			canvas.DrawCircle(new D2Point(0 + h / 2, h / 2), h / 2, frameColor);
+			canvas.DrawCircle(new D2Point(w - h / 2, h / 2), h / 2, frameColor);
+			canvas.FillRect(new I4Rect(h / 2, frame * 0, w - h, h - frame * 0), frameColor);
+			canvas.FillRect(new I4Rect(h / 2, frame * 1, w - h, h - frame * 2), backColor);
+
+			{
+				Func<I4Color, I4Color> a_fill = col =>
+				{
+					col.R /= 2;
+					col.G /= 2;
+					col.B /= 2;
+
+					return col;
+				};
+
+				canvas.FillRect(new I4Rect(1 * w / 2, 0 * h / 2, w / 2, h / 2), a_fill);
+				canvas.FillRect(new I4Rect(0 * w / 2, 1 * h / 2, w / 2, h / 2), a_fill);
+			}
+
+			string name = text;
+
+			{
+				int p = text.IndexOf(':');
+
+				if (p != -1)
+				{
+					name = text.Substring(0, p);
+					text = text.Substring(p + 1);
+				}
+			}
+
+			canvas = canvas.DrawString(text, 180, "メイリオ", FontStyle.Regular, textColor, h / 2 + text_x, 70);
+			canvas = canvas.Expand(w / 12, h / 12);
+			canvas.Save(Path.Combine(Common.GetOutputDir(), name + ".png"));
 		}
 	}
 }
