61411f1bcd92fb876ea0c2f46e9657cd8c463425
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-07-21 09:54:18 +0900

    Voyager 1 is 22959219982.834 kilometers away from the Sun.

--- t20210721_Tests/Claes20200001/Claes20200001/Tests/Test0001.cs
+++ t20210721_Tests/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -11,7 +11,48 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			Test_SCommon_Join();
+			Test_SCommon_LinesToText();
+			Test_SCommon_IndexOf();
+			Test_SCommon_IndexOfIgnoreCase();
+			Test_CsvFileWriter_WriteCells();
+			Test_CsvFileWriter_WriteRow();
+			Test_CsvFileWriter_WriteRows();
+		}
+
+		private void Test_SCommon_Join()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Test_SCommon_LinesToText()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Test_SCommon_IndexOf()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Test_SCommon_IndexOfIgnoreCase()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Test_CsvFileWriter_WriteCells()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Test_CsvFileWriter_WriteRow()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Test_CsvFileWriter_WriteRows()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
