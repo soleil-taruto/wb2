5ee560c1f14ed7534dbb08ae2717a0d4cdaa1902
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-09-13 01:28:24 +0900

    Voyager 1 is 23037549729.726 kilometers away from the Sun.

---
+++ t20210912_SimpleDatabase/Claes20200001/Claes20200001/SimpleDatabases/SimpleDatabase.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Charlotte.Commons;
+
+namespace Charlotte.SimpleDatabases
+{
+	public class SimpleDatabase
+	{
+		private string RootDir;
+		private long FileSizeLimit;
+
+		public SimpleDatabase(string rootDir, long fileSizeLimit)
+		{
+			if (rootDir == null)
+				throw new ArgumentNullException();
+
+			if (fileSizeLimit < 1L || SCommon.IMAX_64 < fileSizeLimit)
+				throw new ArgumentOutOfRangeException();
+
+			rootDir = SCommon.MakeFullPath(rootDir);
+
+			SCommon.CreateDir(rootDir);
+
+			this.RootDir = rootDir;
+			this.FileSizeLimit = fileSizeLimit;
+		}
+
+		public void Add(string[] row)
+		{
+			row = RowFilter(row);
+
+			string[] files = this.GetFiles();
+			string file;
+			bool append = true;
+
+			if (files.Length == 0)
+			{
+				file = this.GetFirstFilePath();
+				append = false;
+			}
+			else
+			{
+				file = files[files.Length - 1];
+
+				FileInfo fileInfo = new FileInfo(file);
+
+				if (this.FileSizeLimit < fileInfo.Length)
+				{
+					file = this.CreateNextFilePath(file);
+					append = false;
+				}
+			}
+
+			using (CsvFileWriter writer = new CsvFileWriter(file, append))
+			{
+				writer.WriteRow(row);
+			}
+		}
+
+		public IEnumerable<string[]> ReadToEnd()
+		{
+			string[] files = this.GetFiles();
+
+			foreach (string file in files)
+			{
+				if (File.Exists(file)) // 読み込み中に削除される場合あり -- this.ReadToEnd() 読み込み中に this.Remove();
+				{
+					string[][] rows = ReadFile(file);
+
+					foreach (string[] row in rows)
+						yield return row;
+				}
+			}
+		}
+
+		public void Remove(Predicate<string[]> match)
+		{
+			string[] files = this.GetFiles();
+
+			foreach (string file in files)
+			{
+				if (File.Exists(file)) // 読み込み中に削除される場合あり -- this.Remove() 中に this.Remove();
+				{
+					using (CsvFileReader reader = new CsvFileReader(file, SCommon.ENCODING_SJIS))
+					{
+						string[][] rows = reader.ReadToEnd();
+						bool removed = false;
+
+						for (int index = 0; index < rows.Length; index++)
+						{
+							if (match(rows[index]))
+							{
+								rows[index] = null;
+								removed = true;
+							}
+						}
+						if (removed)
+						{
+							rows = rows.Where(row => row != null).ToArray();
+
+							if (rows.Length == 0)
+							{
+								SCommon.DeletePath(file);
+							}
+							else
+							{
+								WriteFile(file, rows);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		public void Edit(Func<string[], string[]> editor)
+		{
+			string[] files = this.GetFiles();
+
+			foreach (string file in files)
+			{
+				if (File.Exists(file)) // 読み込み中に削除される場合あり -- this.Edit() 中に this.Remove();
+				{
+					using (CsvFileReader reader = new CsvFileReader(file, SCommon.ENCODING_SJIS))
+					{
+						string[][] rows = reader.ReadToEnd();
+						bool edited = false;
+
+						for (int index = 0; index < rows.Length; index++)
+						{
+							string[] row = editor(rows[index]);
+
+							if (row != null)
+							{
+								row = RowFilter(row);
+								rows[index] = row;
+								edited = true;
+							}
+						}
+						if (edited)
+						{
+							WriteFile(file, rows);
+						}
+					}
+				}
+			}
+		}
+
+		private string[] GetFiles()
+		{
+			return Directory.GetFiles(this.RootDir, "*.csv", SearchOption.TopDirectoryOnly).OrderBy(SCommon.Comp).ToArray();
+		}
+
+		private string GetFirstFilePath()
+		{
+			return Path.Combine(this.RootDir, "20190101000059000.csv");
+		}
+
+		private string CreateNextFilePath(string file)
+		{
+			long counter = long.Parse(Path.GetFileNameWithoutExtension(file));
+			counter++;
+			return Path.Combine(this.RootDir, (counter + 1) + ".csv");
+		}
+
+		private static string[] RowFilter(string[] row)
+		{
+			return row.Select(cell => SCommon.ToJString(SCommon.ENCODING_SJIS.GetBytes(cell), true, true, true, true)).ToArray();
+		}
+
+		private static string[][] ReadFile(string file)
+		{
+			using (CsvFileReader reader = new CsvFileReader(file))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+
+		private static void WriteFile(string file, string[][] rows)
+		{
+			string midFile = CreateTempFile(file + "-mid-");
+			string delFile = CreateTempFile(file + "-del-");
+
+			using (CsvFileWriter writer = new CsvFileWriter(midFile))
+			{
+				writer.WriteRows(rows);
+			}
+			File.Move(file, delFile);
+			File.Move(midFile, file);
+			SCommon.DeletePath(delFile);
+		}
+
+		private static string CreateTempFile(string prefix)
+		{
+			long counter = SCommon.SimpleDateTime.Now().ToTimeStamp() * 1000L;
+
+			for (; ; )
+			{
+				string file = prefix + counter + ".tmp";
+
+				if (!File.Exists(file))
+					return file;
+
+				counter++;
+			}
+		}
+	}
+}
