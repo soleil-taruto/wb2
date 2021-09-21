21614ba2f9e9be66602b21fbe85ac77f3a9554bc
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-09-21 09:00:40 +0900

    Voyager 1 is 23049688170.684 kilometers away from the Sun.

---
+++ t20210922_SimplePaint/Silvia20200001/Silvia20200001/MainWin.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using Charlotte.Commons;
+
+namespace Charlotte
+{
+	public partial class MainWin : Form
+	{
+		[SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+		protected override void WndProc(ref Message m)
+		{
+			const int WM_SYSCOMMAND = 0x112;
+			const long SC_CLOSE = 0xF060L;
+
+			if (m.Msg == WM_SYSCOMMAND && (m.WParam.ToInt64() & 0xFFF0L) == SC_CLOSE)
+			{
+				this.BeginInvoke((MethodInvoker)this.CloseWindow);
+				return;
+			}
+			base.WndProc(ref m);
+		}
+
+		public MainWin()
+		{
+			InitializeComponent();
+
+			this.MinimumSize = this.Size;
+		}
+
+		private void MainWin_Load(object sender, EventArgs e)
+		{
+			// init WriteLog
+			{
+				string consoleLogFile = ProcMain.SelfFile + "-console.log";
+
+				File.WriteAllBytes(consoleLogFile, SCommon.EMPTY_BYTES);
+
+				ProcMain.WriteLog = message =>
+				{
+					try
+					{
+						File.AppendAllLines(consoleLogFile, new string[] { "[" + DateTime.Now + "] " + message }, Encoding.UTF8);
+					}
+					catch
+					{ }
+				};
+			}
+
+			Ground.I = new Ground();
+		}
+
+		private void MainWin_Shown(object sender, EventArgs e)
+		{
+			// none
+		}
+
+		private void MainWin_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			// none
+		}
+
+		private void MainWin_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			// none
+		}
+
+		private void CloseWindow()
+		{
+			this.Close();
+		}
+	}
+}
