a6a44af7422a40933530f65b716577581ee75ccc
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-09-27 10:09:33 +0900

    Voyager 1 is 23058517731.180 kilometers away from the Sun.

--- OneTime/t20210324_Template/Claes20200001/Claes20200001/Commons/RandomUnit.cs
+++ OneTime/t20210324_Template/Claes20200001/Claes20200001/Commons/RandomUnit.cs
@@ -100,7 +100,7 @@ namespace Charlotte.Commons
 		public ulong GetUInt64_M(ulong modulo)
 		{
 			if (modulo == 0ul)
-				throw new Exception("modulo is zero"); // ConfuserElsa のために ArgumentOutOfRangeException を使用しない。
+				throw new ArgumentOutOfRangeException("modulo is zero");
 
 			ulong m = (ulong.MaxValue % modulo + 1ul) % modulo;
 			ulong r;
