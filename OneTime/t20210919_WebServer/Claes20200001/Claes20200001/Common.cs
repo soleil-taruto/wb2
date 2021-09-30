fdd75d2c719a9f295580b147bf4acfd6b1ba8fee
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-09-30 09:36:57 +0900

    Voyager 1 is 23062864692.126 kilometers away from the Sun.

---
+++ OneTime/t20210919_WebServer/Claes20200001/Claes20200001/Common.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using Charlotte.Commons;
+
+namespace Charlotte
+{
+	public static class Common
+	{
+		public static void Pause()
+		{
+			Console.WriteLine("Press ENTER key.");
+			Console.ReadLine();
+		}
+
+		#region GetOutputDir
+
+		private static string GOD_Dir;
+
+		public static string GetOutputDir()
+		{
+			if (GOD_Dir == null)
+				GOD_Dir = GetOutputDir_Main();
+
+			return GOD_Dir;
+		}
+
+		private static string GetOutputDir_Main()
+		{
+			for (int c = 1; c <= 999; c++)
+			{
+				string dir = "C:\\" + c;
+
+				if (
+					!Directory.Exists(dir) &&
+					!File.Exists(dir)
+					)
+				{
+					SCommon.CreateDir(dir);
+					//SCommon.Batch(new string[] { "START " + dir });
+					return dir;
+				}
+			}
+			throw new Exception("C:\\1 ～ 999 は使用できません。");
+		}
+
+		public static void OpenOutputDir()
+		{
+			SCommon.Batch(new string[] { "START " + GetOutputDir() });
+		}
+
+		public static void OpenOutputDirIfCreated()
+		{
+			if (GOD_Dir != null)
+			{
+				OpenOutputDir();
+			}
+		}
+
+		private static int NOP_Count = 0;
+
+		public static string NextOutputPath()
+		{
+			return Path.Combine(GetOutputDir(), (++NOP_Count).ToString("D4"));
+		}
+
+		#endregion
+
+		public static double GetDistance(D2Point pt)
+		{
+			return Math.Sqrt(pt.X * pt.X + pt.Y * pt.Y);
+		}
+
+		public static string[] ParseIsland(string text, string singleTag, bool ignoreCase = false)
+		{
+			int start;
+
+			if (ignoreCase)
+				start = text.ToLower().IndexOf(singleTag.ToLower());
+			else
+				start = text.IndexOf(singleTag);
+
+			if (start == -1)
+				return null;
+
+			int end = start + singleTag.Length;
+
+			return new string[]
+			{
+				text.Substring(0, start),
+				text.Substring(start, end - start),
+				text.Substring(end),
+			};
+		}
+
+		public static string[] ParseEnclosed(string text, string openTag, string closeTag, bool ignoreCase = false)
+		{
+			string[] starts = ParseIsland(text, openTag, ignoreCase);
+
+			if (starts == null)
+				return null;
+
+			string[] ends = ParseIsland(starts[2], closeTag, ignoreCase);
+
+			if (ends == null)
+				return null;
+
+			return new string[]
+			{
+				starts[0],
+				starts[1],
+				ends[0],
+				ends[1],
+				ends[2],
+			};
+		}
+	}
+}
