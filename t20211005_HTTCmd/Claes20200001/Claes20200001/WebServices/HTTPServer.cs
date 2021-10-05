3fca81415f68ec45bd71d1cb62f88cb76a09c343
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-10-05 20:42:50 +0900

    Voyager 1 is 23070839589.936 kilometers away from the Sun.

---
+++ t20211005_HTTCmd/Claes20200001/Claes20200001/WebServices/HTTPServer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.WebServices
+{
+	public class HTTPServer : SockServer
+	{
+		/// <summary>
+		/// サーバーロジック
+		/// 引数：
+		/// -- channel: 接続チャネル
+		/// </summary>
+		public Action<HTTPServerChannel> HTTPConnected = channel => { };
+
+		// <---- prm
+
+		public HTTPServer()
+		{
+			PortNo = 80;
+		}
+
+		protected override IEnumerable<int> E_Connected(SockChannel channel)
+		{
+			HTTPServerChannel hsChannel = new HTTPServerChannel();
+			int retval = -1;
+
+			hsChannel.Channel = channel;
+
+			foreach (int size in hsChannel.RecvRequest())
+			{
+				if (size == 0)
+					throw null; // never
+
+				if (size < 0)
+				{
+					yield return retval;
+					retval = -1;
+				}
+				else
+					retval = 1;
+			}
+
+			SockCommon.NB("svlg", () =>
+			{
+				HTTPConnected(hsChannel);
+				return -1; // dummy
+			});
+
+			foreach (int size in hsChannel.SendResponse())
+			{
+				if (size == 0)
+					throw null; // never
+
+				if (size < 0)
+				{
+					yield return retval;
+					retval = -1;
+				}
+				else
+					retval = 1;
+			}
+		}
+	}
+}
