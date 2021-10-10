a26a011b7606b73e83923ee8036006c67d62e942
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-10-11 03:56:46 +0900

    Voyager 1 is 23078579221.890 kilometers away from the Sun.

--- OneTime/t20210721_Tests/Claes20200001/Claes20200001/Program.cs
+++ OneTime/t20210721_Tests/Claes20200001/Claes20200001/Program.cs
@@ -53,7 +53,8 @@ namespace Charlotte
 			//new Test0003().Test05();
 			//new Test0003().Test05_2();
 			//new Test0003().Test05_3();
-			new Test0003().Test05_4();
+			//new Test0003().Test05_4();
+			new Test0003().Test05_5();
 
 			// --
 		}
