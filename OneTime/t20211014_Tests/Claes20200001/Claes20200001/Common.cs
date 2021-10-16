cc4a1904fad056309a7ccdfc43031ba4153782bc
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-10-16 14:34:52 +0900

    Voyager 1 is 23086525882.637 kilometers away from the Sun.

--- OneTime/t20211014_Tests/Claes20200001/Claes20200001/Common.cs
+++ OneTime/t20211014_Tests/Claes20200001/Claes20200001/Common.cs
@@ -74,26 +74,50 @@ namespace Charlotte
 			return Math.Sqrt(pt.X * pt.X + pt.Y * pt.Y);
 		}
 
+		/// <summary>
+		/// 深さ優先探索によるリストの生成
+		/// </summary>
+		/// <typeparam name="T">要素の型</typeparam>
 		public class RecursiveSearch<T>
 		{
-			// TODO
-			// TODO
-			// TODO
-
-
-
+			/// <summary>
+			/// 途中までのリストが間違っているか判定する。
+			/// </summary>
 			public Func<List<T>, bool> IsInvalid = list => false;
 
+			/// <summary>
+			/// リストが完成しているか判定する。
+			/// </summary>
 			public Func<List<T>, bool> IsEnd = list => false;
 
-			public Action<List<T>> Ended = list => { };
+			/// <summary>
+			/// 完成したリストに対するリアクション
+			/// 戻り値：探索を終了するか
+			/// </summary>
+			public Func<List<T>, bool> Ended = list => false;
 
-			public Func<List<T>, T> CreateElement = list => default(T);
+			/// <summary>
+			/// 要素を生成する。
+			/// 要素の取り得る値を e(1), e(2), e(3), ... e(N - 2), e(N - 1), e(N) とすると e(0) を返す。
+			/// </summary>
+			public Func<List<T>, T> CreateZeroThElement = list => default(T);
 
-			public Func<List<T>, T, Action<T>, bool> MoveFirstOrNextElement = (list, element, setter) => { setter(element); return true; };
+			/// <summary>
+			/// 要素の取り得る値を e(1), e(2), e(3), ... e(N - 2), e(N - 1), e(N) とする。
+			/// - - -
+			/// 要素の現在値 != e(N) --> 要素の現在値 == e(i) -- setter(e(i + 1)); return true;
+			/// 要素の現在値 == e(N) --> return false;
+			/// </summary>
+			public Func<List<T>, T, Action<T>, bool> MoveToFirstOrNextElement = (list, element, setter) => { setter(element); return true; };
 
+			/// <summary>
+			/// 要素を解放する。
+			/// </summary>
 			public Action<List<T>, T> ReleaseElement = (list, element) => { };
 
+			/// <summary>
+			/// 探索実行
+			/// </summary>
 			public void Perform()
 			{
 				List<T> list = new List<T>();
@@ -104,13 +128,15 @@ namespace Charlotte
 
 				if (this.IsEnd(list))
 				{
-					this.Ended(list);
+					if (this.Ended(list))
+						return;
+
 					goto back;
 				}
-				list.Add(this.CreateElement(list));
+				list.Add(this.CreateZeroThElement(list));
 
 			next:
-				if (this.MoveFirstOrNextElement(list, list[list.Count - 1], element => list[list.Count - 1] = element))
+				if (this.MoveToFirstOrNextElement(list, list[list.Count - 1], element => list[list.Count - 1] = element))
 					goto forward;
 
 			back:
