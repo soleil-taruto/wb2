ddf2e2840a6c09f564cdf25786f1a022b6c0fc48
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-12-02 23:03:29 +0900

    Voyager 1 is 23155656707.646 kilometers away from the Sun.

--- OneTime/t20211203_Tests_AES/Claes20200001/Claes20200001/Commons/ProcMain.cs
+++ OneTime/t20211203_Tests_AES/Claes20200001/Claes20200001/Commons/ProcMain.cs
@@ -14,7 +14,7 @@ namespace Charlotte.Commons
 {
 	public static class ProcMain
 	{
-		public const string APP_IDENT = "{46863762-dd9a-456d-92f0-5dca7096a705}"; // アプリ毎に変更する。
+		public const string APP_IDENT = "{61c93f76-ceca-4593-8d60-57fef2c6e160}"; // アプリ毎に変更する。
 		public const string APP_TITLE = "Claes20200001";
 
 		public static string SelfFile;
