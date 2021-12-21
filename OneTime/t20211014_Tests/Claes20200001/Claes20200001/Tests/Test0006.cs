54dc5a1192ad90e1c135a4b3881c8a93babe18a5
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-12-22 02:27:48 +0900

    Voyager 1 is 23183600890.610 kilometers away from the Sun.

--- OneTime/t20211014_Tests/Claes20200001/Claes20200001/Tests/Test0006.cs
+++ OneTime/t20211014_Tests/Claes20200001/Claes20200001/Tests/Test0006.cs
@@ -12,15 +12,17 @@ namespace Charlotte.Tests
 		public void Test01()
 		{
 			Test01_a(new string[0]);
+			Test01_a(new string[] { "" });
+			Test01_a(new string[] { "", "" });
+			Test01_a(new string[] { "", "", "" });
+			Test01_a(new string[] { "ABC" });
+			Test01_a(new string[] { "ABC", "DEF" });
+			Test01_a(new string[] { "ABC", "DEF", "GHI" });
 
 			char[] TEST_CHARS = (SCommon.HALF + SCommon.GetJChars()).ToArray();
 
 			for (int testcnt = 0; testcnt < 10000; testcnt++)
 			{
-				// cout
-				if (testcnt % 100 == 0)
-					Console.WriteLine(testcnt);
-
 				string[] testStrings = new string[SCommon.CRandom.GetRange(1, 100)];
 
 				for (int index = 0; index < testStrings.Length; index++)
@@ -41,6 +43,15 @@ namespace Charlotte.Tests
 		{
 			string enc = SCommon.Serializer.I.Join(testStrings);
 			string[] dec = SCommon.Serializer.I.Split(enc);
+
+			// cout
+			{
+				string enc4Prn = enc.Length < 78 ? enc : enc.Substring(0, 30) + " ... " + enc.Substring(enc.Length - 30);
+
+				Console.WriteLine("< " + testStrings.Length);
+				Console.WriteLine("* " + enc4Prn);
+				Console.WriteLine("> " + dec.Length);
+			}
 
 			if (!Regex.IsMatch(enc, "^[A-Za-z0-9+/]{2,}$")) // ? 書式エラー
 				throw null;
